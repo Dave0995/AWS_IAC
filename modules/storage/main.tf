6585d8a4f13633c1a69e5d7ad124fa422b511266
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-12 14:27:08 -0500

    fixed modules

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "airflow-bucket" {
+resource "aws_s3_bucket" "airflow_bucket" {
   bucket = "dave-airflow-bucket-${var.environment}"
 }
 
-resource "aws_s3_bucket" "data-contracts-bucket" {
+resource "aws_s3_bucket" "data_contracts_bucket" {
   bucket = "dave-data-contracts-${var.environment}"
 }
 
-resource "aws_s3_bucket" "raw-data-bucket" {
+resource "aws_s3_bucket" "raw_data_bucket" {
   bucket = "dave-data-lake-customer-${var.environment}"
 }
