ff21077efb51dcc48bfa6843c0506e89822bd42d
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-12 14:27:59 -0500

    implemented storage module for development environment

---
+++ environments/development/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  description = "AWS region where we are going to allocate all resources"
+}
+
+variable "environment" {
+  description = "Initial of the deployment environment name"
+}
