36a1953259bf7ee856748ba9e06eaf0888bd6d8e
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-12 17:16:17 -0500

    added type key in variables

---
+++ modules/network/variables.tf
@@ -0,0 +1,9 @@
+variable "availability_zone" {
+    type = string
+    description = "Availability zone for the VPC network"
+}
+
+variable "vpc_tag_name" {
+    type = string
+    description = "Tag name for the VPC network"
+}
