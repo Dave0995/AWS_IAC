18beff973a03cc9109ab9568cba90fa88dbe3349
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-15 15:51:57 -0500

    added variables for setting up the module

---
+++ modules/orchestrator/variables.tf
@@ -0,0 +1,13 @@
+variable "security_group_id" {
+    type = string
+    description = "ID for the VPC security group"
+}
+
+variable "s3_airflow_bucket_arn" {
+    description = "S3 bucket for airflow instance" 
+}
+
+variable "private_subnets_id" {
+    type = list(string)
+    description = "private subnets neede for the MWAA instance"
+}
