e7b0e70a66b81c48f09ec865db83e3b90a17a329
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-15 17:06:19 -0500

    last working

--- modules/orchestrator/main.tf
+++ modules/orchestrator/main.tf
@@ -21,17 +21,96 @@ resource "aws_iam_role_policy" "mwaa_execution_role_policy" {
 
   policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "airflow:CreateEnvironment",
-      "Resource": "arn:aws:airflow:us-east-1:975050150843:environment/example"
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "airflow:PublishMetrics",
+            "Resource": "arn:aws:airflow:us-east-1:975050150843:environment/MyAirflowEnvironment"
+        },
+        {
+            "Effect": "Deny",
+            "Action": "s3:ListAllMyBuckets",
+            "Resource": [
+                "arn:aws:s3:::dave-airflow-bucket-d",
+                "arn:aws:s3:::dave-airflow-bucket-d/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject*",
+                "s3:GetBucket*",
+                "s3:List*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::dave-airflow-bucket-d",
+                "arn:aws:s3:::dave-airflow-bucket-d/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:GetLogRecord",
+                "logs:GetLogGroupFields",
+                "logs:GetQueryResults"
+            ],
+            "Resource": [
+                "arn:aws:logs:us-east-1:975050150843:log-group:airflow-MyAirflowEnvironment-*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:DescribeLogGroups"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:PutMetricData",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sqs:ChangeMessageVisibility",
+                "sqs:DeleteMessage",
+                "sqs:GetQueueAttributes",
+                "sqs:GetQueueUrl",
+                "sqs:ReceiveMessage",
+                "sqs:SendMessage"
+            ],
+            "Resource": "arn:aws:sqs:us-east-1:*:airflow-celery-*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt",
+                "kms:DescribeKey",
+                "kms:GenerateDataKey*",
+                "kms:Encrypt"
+            ],
+            "NotResource": "arn:aws:kms:*:975050150843:key/*",
+            "Condition": {
+                "StringLike": {
+                    "kms:ViaService": [
+                        "sqs.us-east-1.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
 }
 EOF
 }
+
 
 
 resource "aws_mwaa_environment" "airflow_instance" {
