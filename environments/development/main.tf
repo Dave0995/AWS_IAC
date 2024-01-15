80459ed3d8f27c8a09ef2a73dc33e3a954593022
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-15 13:33:38 -0500

    implemented variables on tfvars file

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -20,24 +20,23 @@ module "dave_network" {
   source = "../../modules/network"
   
   # VPC
-  vpc_cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  vpc_tag_name = "dave-vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  enable_dns_support = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  vpc_tag_name = var.vpc_tag_name
 
   # Public subnet
-  public_subnet_cidr_block = "10.0.1.0/24"
-  public_availability_zone = "us-east-1a"
-  public_map_public_ip = true
-  public_subnet_tag_name = "dave-public-subnet"
+  public_subnet_cidr_block = var.public_subnet_cidr_block
+  public_availability_zone = var.public_availability_zone
+  public_map_public_ip = var.public_map_public_ip
+  public_subnet_tag_name = var.public_subnet_tag_name
 
   # Private subnet
-  private_subnet_cidr_block = "10.0.2.0/24"
-  private_availability_zone = "us-east-1b"
-  private_map_public_ip = false
-  private_subnet_tag_name = "dave-private-subnet"
+  private_subnet_cidr_block = var.private_subnet_cidr_block
+  private_availability_zone = var.private_availability_zone
+  private_map_public_ip = var.private_map_public_ip
+  private_subnet_tag_name = var.private_subnet_tag_name
 
-  internet_gateway_tag = "dave-igw"
-  public_route_table_tag = "dave-route-table"
-
+  internet_gateway_tag = var.internet_gateway_tag
+  public_route_table_tag = var.public_route_table_tag
 }
