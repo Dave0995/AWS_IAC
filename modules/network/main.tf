3d94d8836ad1048c9d4298d3967fad69db940f8f
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-15 09:41:40 -0500

    network setup includen igw and both public and private subnets

---
+++ modules/network/main.tf
@@ -0,0 +1,52 @@
+resource "aws_vpc" "dave_vpc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags = {
+    Name = var.vpc_tag_name
+  }
+}
+
+resource "aws_subnet" "dave_public_subnet" {
+  vpc_id                  = aws_vpc.dave_vpc.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.public_availability_zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = var.public_subnet_tag_name
+  }
+}
+
+resource "aws_subnet" "dave_private_subnet" {
+  vpc_id                  = aws_vpc.dave_vpc.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.private_availability_zone
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.private_subnet_tag_name
+  }
+}
+
+resource "aws_internet_gateway" "dave_igw" {
+  vpc_id = aws_vpc.dave_vpc.id
+  tags = {
+    Name = var.internet_gateway_tag
+  }
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "dave_public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dave_igw.id
+  }
+  tags = {
+    Name = var.public_route_table_tag
+  }
+}
+
+resource "aws_security_group" "my_security_group" {
+  vpc_id = aws_vpc.dave_vpc.id
+}
+
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,9 +1,54 @@
-variable "availability_zone" {
+variable "public_availability_zone" {
     type = string
-    description = "Availability zone for the VPC network"
+    description = "Availability zone for the public VPC subnet"
+}
+
+variable "private_availability_zone" {
+    type = string
+    description = "Availability zone for the private VPC subnet"
 }
 
 variable "vpc_tag_name" {
     type = string
     description = "Tag name for the VPC network"
+}
+
+variable "public_subnet_tag_name" {
+    type = string
+    description = "Tag name for the public subnet"
+}
+
+variable "private_subnet_tag_name" {
+    type = string
+    description = "Tag name for the private subnet"
+}
+
+variable "internet_gateway_tag" {
+    type = string
+    description = "Tag name for the internet gatway"
+}
+
+variable "public_route_table_tag" {
+    type = string
+    description = "Tag name for public route table"
+}
+
+variable "vpc_cidr_block" {
+    type = string
+    description = "IP range values for vpc"
+}
+
+variable "subnet_cidr_block" {
+    type = string
+    description = "IP range values for subnet"
+}
+
+variable "enable_dns_support" {
+    type = bool
+    description = "Flag for adding the support to DNS"
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    description = "Flag for enabling dns hostnames"
 }
