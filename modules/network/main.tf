02380ec67fe4e3efdc945fa86923b3ca173bae2a
Dave Rodriguez <dave.rodriguez@atlas.health>
2024-01-16 11:17:44 -0500

    added ingress and egress rules to the security group

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -58,6 +58,20 @@ resource "aws_route_table" "dave_public_route_table" {
 resource "aws_security_group" "dave_security_group" {
   vpc_id = aws_vpc.dave_vpc.id
 
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     name = "${var.aws_security_group_tag}-${var.environment}"
   }
